b75ada24f9cfddf2e057638743e6f34d678f77bf
lvgx <l@vgx.fr>
2020-12-23 20:12:02 +0100

    Fix build
    Tested on gcc 10.2.0 (linux)

--- src/pilot.h
+++ src/pilot.h
@@ -34,7 +34,7 @@ struct Ship;
 
 #define PARACHUTE_FRAME 2
 
-struct Pilot {
+typedef struct Pilot {
     struct Walker walker;       /* inherits Walker */
     SDL_Surface *sprite[3];     /* Normal,Normal2, Parachute */
     Vector attack_vector;       /* Direction where gun is pointed to */
